c26704c9567108d90c5249805961dfa3153add54
Sally Hay <sally@pikopiko.com.au>
2018-04-21 12:39:49 +1000

    Second commit mark 2
    Removed css and <script> info relating to slick and js

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+    $(document).ready(function() {
+      $('.carousel').slick({
+		slidesToScroll: 1,
+        dots: false,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		draggable: false,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+      });
+    });
+
